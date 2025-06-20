74f5c143cad1d2d9f7189ec016b9b76a5b8eea1a
JieJiang1697918 <165370396+JieJiang1697918@users.noreply.github.com>
2025-06-20 12:20:48 +0000

    [automatic commit] 2025-06-20 12:20:48 UTC 4.147.189.193

--- part1/app.js
+++ part1/app.js
@@ -87,7 +87,7 @@ let db;
   }
 })();
 
-        const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
+    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
       await db.execute(`
         INSERT INTO Users (username, email, password_hash, role) VALUES
