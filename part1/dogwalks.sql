0aa6334b3575413bd81835ebd0bd40213b7d45b4
JieJiang1697918 <165370396+JieJiang1697918@users.noreply.github.com>
2025-06-20 08:15:43 +0000

    [automatic commit] 2025-06-20 08:15:43 UTC 68.218.39.192

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -70,3 +70,10 @@ VALUES
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Banana', 'small'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Cake', 'medium');
 
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+VALUES
+((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+((SELECT dog_id FROM Dogs WHERE name = 'Rocky'), '2025-06-11 07:15:00', 60, 'Lakeside Trail', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Milo'), '2025-06-12 10:00:00', 40, 'Botanic Garden', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Daisy'), '2025-06-13 17:30:00', 20, 'City Park', 'cancelled');
