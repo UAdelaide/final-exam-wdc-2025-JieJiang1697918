fb7d453aa473f3de6c3421883d349eed1e6ab018
JieJiang1697918 <165370396+JieJiang1697918@users.noreply.github.com>
2025-06-20 12:32:50 +0000

    [automatic commit] 2025-06-20 12:32:50 UTC 4.147.189.193

---
+++ part1/db.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'wdcproject'
+});
+
+module.exports = pool;
