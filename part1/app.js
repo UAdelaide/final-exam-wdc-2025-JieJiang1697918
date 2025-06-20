372537fbdb6e0a17414764628d4c5e9e0e073195
JieJiang1697918 <165370396+JieJiang1697918@users.noreply.github.com>
2025-06-20 12:43:52 +0000

    [automatic commit] 2025-06-20 12:43:52 UTC 4.147.189.193

--- part1/app.js
+++ part1/app.js
@@ -14,7 +14,19 @@ app.use(cookieParser());
 
 testdata();
 
-
+app.get('/api/dogs', async (req, res) => {
+  try {
+    const [rows] = await pool.query(`
+      SELECT d.dog_id, d.name AS dog_name, d.size, u.username AS owner_username
+      FROM Dogs d
+      JOIN Users u ON d.owner_id = u.user_id
+    `);
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
