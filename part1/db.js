6482c277be158ba0db6fb5b489b9f4e02edbe773
JieJiang1697918 <165370396+JieJiang1697918@users.noreply.github.com>
2025-06-20 12:33:10 +0000

    [automatic commit] 2025-06-20 12:33:10 UTC 4.147.189.193

--- part1/db.js
+++ part1/db.js
@@ -3,7 +3,7 @@ const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
     password: '',
-    database: 'wdcproject'
+    database: 'DogWalkService'
 });
 
 module.exports = pool;
