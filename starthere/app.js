a05082dfee51993bfb2be4b9641ee5cfa5c741dc
JieJiang1697918 <165370396+JieJiang1697918@users.noreply.github.com>
2025-06-20 12:18:08 +0000

    [automatic commit] 2025-06-20 12:18:08 UTC 4.147.189.193

--- starthere/app.js
+++ starthere/app.js
@@ -47,16 +47,21 @@ let db;
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
-      `);
+        INSERT INTO Users (username, email, password_hash, role)
+        VALUES
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('stevewalker', 'steve@example.com', 'hashed147', 'walker'),
+        ('jimmy123', 'jimmy@example.com', 'hashed369', 'owner');
+              `);
     }
   } catch (err) {
     console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
   }
 })();
+
+
 
 // Route to return books as JSON
 app.get('/', async (req, res) => {
