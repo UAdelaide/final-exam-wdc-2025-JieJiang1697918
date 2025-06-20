805391e14cb6889f74b588c01149944a480d450a
JieJiang1697918 <165370396+JieJiang1697918@users.noreply.github.com>
2025-06-20 08:09:42 +0000

    [automatic commit] 2025-06-20 08:09:42 UTC 68.218.39.192

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -59,5 +59,5 @@ VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('davidwalker', 'david@example.com', 'hashdavid', 'walker'),
-('emilyowner', 'emily@example.com', 'hashemily', 'owner');
+('carlwalker', 'david@example.com', 'hashed', 'walker'),
+('lipowner', 'emily@example.com', 'hashemily', 'owner');
